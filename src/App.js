46d420ee3f573f078a2e4555e18700b2cbeaf5a4
GFP <fealx15@gmail.com>
2024-09-23 16:39:57 +0700

    f

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import { BackButton, initBackButton, parseInitData} from '@telegram-apps/sdk';
+import {initBackButton, parseInitData} from '@telegram-apps/sdk';
 import {SDKProvider} from "@telegram-apps/sdk-react";
 
 function App() {
     const [backButton] = initBackButton();
-    const initDataString = window.location.search.slice(1);
+    const url = new URL(window.location.href);
+    const initDataString = url.searchParams.get('#tgWebAppData')
+    console.log(initDataString);
     const initData = parseInitData(initDataString);
     backButton.show();
     return (
         <SDKProvider acceptCustomStyles debug>
             <div>My application!</div>
-            <span>{parseInitData(initDataString)}</span>
+            <span>{initData}</span>
         </SDKProvider>
     );
 }
