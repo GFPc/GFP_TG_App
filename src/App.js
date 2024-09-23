99c0e01ecb5bd25b5f30d2c14553cab167eafe26
GFP <fealx15@gmail.com>
2024-09-23 17:32:30 +0700

    f

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import {SDKProvider} from "@telegram-apps/sdk-react";
 
 function App() {
     const [backButton] = initBackButton();
-    const initDataString =
-        'query_id=AAHdF6IQAAAAAN0XohDhrOrc' +
-        '&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D' +
-        '&auth_date=1662771648' +
-        '&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2';
+    var url = window.location.href.split("#")[1];
+    url = new URLSearchParams(url)
+    url = url.get("tgWebAppData")
+    console.log(url)
+
+
+    const initDataString = url
+    //console.log(initDataString)
     const initData = parseInitData(initDataString);
     backButton.show();
     return (
